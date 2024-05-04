70965fc50c78c6026f7ab15c950cc9eb4933df72
Marc Marcos Madruga <marcmarcosmadruga@gmail.com>
2024-05-04 14:21:14 +0200

    changed file structure

--- frontend/app.py
+++ frontend/app.py
@@ -7,4 +7,17 @@ app = Flask(__name__, static_url_path="/static", static_folder="static")
 
 @app.route("/login_view")
 def hello_world():
-    return app.send_static_file("index.html")
+    return app.send_static_file("login.html")
+
+@app.route("/your_flights")
+def your_flights():
+    print("test")
+    return app.send_static_file("YourFlights/yourflights.html")
+
+@app.route("/registrar_no_vidente")
+def registrar_no_vidente():
+    return app.send_static_file("RegistrarNoVidente/RegistrarNoVidente.html")
+
+@app.route("/")
+def index():
+    return app.send_static_file("home/Pag_inicio.html")
